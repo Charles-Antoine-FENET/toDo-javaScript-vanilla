1348e7efee2805f21b4a7b393f79a2468590135b
Charles-Antoine FENET <108330821+Charles-Antoine-FENET@users.noreply.github.com>
2023-04-10 15:25:24 +0200

    add delete item

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "./style.css";
 
 const ul = document.querySelector("ul");
+const form = document.querySelector("form");
+const input = document.querySelector("form > input");
 
 const todos = [
   {
@@ -13,6 +15,13 @@ const todos = [
   }
 ];
 
+form.addEventListener("submit", event => {
+  event.preventDefault();
+  const value = input.value;
+  input.value = "";
+  addTodo(value);
+});
+
 const displayTodo = () => {
   const todosNode = todos.map((todo, index) => {
     return createTodoElement(todo, index);
@@ -23,11 +32,30 @@ const displayTodo = () => {
 
 const createTodoElement = (todo, index) => {
   const li = document.createElement("li");
+  const buttonDelete = document.createElement("button");
+  buttonDelete.innerHTML = "Supprimer";
+  buttonDelete.addEventListener("click", event => {
+    deleteTodo(index);
+  });
   li.innerHTML = `
     <span class="todo ${todo.done ? "done" : ""}"></span>
     <p>${todo.text}</p>
   `;
+  li.appendChild(buttonDelete);
   return li;
+};
+
+const addTodo = text => {
+  todos.push({
+    text,
+    done: false
+  });
+  displayTodo();
+};
+
+const deleteTodo = index => {
+  todos.splice(index, 1);
+  displayTodo();
 };
 
 displayTodo();
